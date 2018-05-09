2ba89cc51fb0d2a78e132af959614ddb04eb4338
KwatME <kwatme8@gmail.com>
2018-05-09 02:05:51 -0700

    spro share 0.0.2

---
+++ code/environment.py
@@ -0,0 +1,63 @@
+import os
+
+import pandas as pd
+
+# import ccal
+import sys
+sys.path.insert(0, '/Users/k/Jumis/github_ccal/ccal')
+import ccal
+
+def make_match_panels(target_x_sample, feature_x_sample, n_job,
+                      extreme_feature_threshold, to_peek, title_prefix, directory_path):
+
+    for index, target in target_x_sample.iterrows():
+
+        print(index)
+
+        to_keep = target != -1
+
+        target = target[to_keep]
+
+        file_path_prefix = '{}/{}'.format(directory_path, index)
+
+        scores_file_path = '{}.match_panel.tsv'.format(file_path_prefix)
+
+        if os.path.isfile(scores_file_path):
+
+            scores = pd.read_table(scores_file_path, index_col=0)
+
+        else:
+
+            scores = None
+
+        if target.unique().size == 2:
+
+            target_type = 'binary'
+
+        elif 2 < target.unique().size:
+
+            target_type = 'categorical'
+            
+        title = '{}{}'.format(title_prefix, index)
+
+        scores = ccal.make_match_panel(
+            target,
+            feature_x_sample,
+            scores=scores,
+            n_job=n_job,
+            extreme_feature_threshold=extreme_feature_threshold,
+            title=title,
+            target_type=target_type,
+            file_path_prefix=file_path_prefix)
+
+        common_indices = feature_x_sample.index & set(to_peek)
+
+        if 0 < len(common_indices):
+
+            ccal.make_match_panel(
+                target,
+                feature_x_sample.loc[common_indices],
+                scores=scores.loc[common_indices],
+                extreme_feature_threshold=None,
+                title=title,
+                target_type=target_type)
---
+++ code/parameters.py
@@ -0,0 +1,39 @@
+GENES_TO_PEEK = (
+    'CCND1',
+    'CTGF',
+    'CYR61',
+    'DUSP6',
+    'TRIB3',
+    'AMOT',
+    'AMOTL1',
+    'AMOTL2',
+    'DCHS1',
+    'DCHS2',
+    'FAT1',
+    'FAT2',
+    'FAT3',
+    'FAT4',
+    'FRMD6',
+    'LATS1',
+    'LATS2',
+    'NF2',
+    'SAV1',
+    'STK3',
+    'STK4',
+    'TEAD1',
+    'TEAD2',
+    'TEAD3',
+    'TEAD4',
+    'WWC1',
+    'WWTR1',
+    'YAP1', )
+
+GENE_SETS_TO_PEEK = ()
+
+GENE_SET_KEYWORDS = ('yap', 'hippo', )
+
+N_JOB = 2
+
+GSEA_NORMALIZATION_METHOD = '0-1'
+
+GSEA_STATISTIC = 'auc'
