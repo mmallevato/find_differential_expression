50058b8cac5db8ba4a62baba7e889aac30cdfd90
KwatME <kwatme8@gmail.com>
2018-05-09 13:58:23 -0700

    0

--- code/environment.py
+++ code/environment.py
@@ -25,7 +25,7 @@ def make_match_panels(target_x_sample, feature_x_sample, n_job,
         if os.path.isfile(scores_file_path):
 
             scores = pd.read_table(scores_file_path, index_col=0)
-
+            
         else:
 
             scores = None
@@ -39,7 +39,7 @@ def make_match_panels(target_x_sample, feature_x_sample, n_job,
             target_type = 'categorical'
             
         title = '{}{}'.format(title_prefix, index)
-
+        
         scores = ccal.make_match_panel(
             target,
             feature_x_sample,
@@ -49,11 +49,11 @@ def make_match_panels(target_x_sample, feature_x_sample, n_job,
             title=title,
             target_type=target_type,
             file_path_prefix=file_path_prefix)
-
-        common_indices = feature_x_sample.index & set(to_peek)
+        
+        common_indices = feature_x_sample.index & scores.index & set(to_peek)
 
         if 0 < len(common_indices):
-
+            
             ccal.make_match_panel(
                 target,
                 feature_x_sample.loc[common_indices],
