73208e87b83c422d7760f1cf4c7f301fa36181bf
KwatME <kwatme8@gmail.com>
2018-06-26 15:43:48 -0700

    spro share 0.0.8

---
+++ code/analysis.py
@@ -0,0 +1,35 @@
+GENES_TO_PEEK = (
+    'SOX2',
+    'POU5F1',
+    'MYC',
+    'KLF4',
+    'NANOG',
+    'ABCG2',
+    'LGR4',
+    'CD34',
+    'CD44',
+    'ZSCAN4',
+    'BMI1',
+    'HIF1A',
+    'EZH2',
+    'NES',
+    'TWIST1',
+    'EPAS1',
+    'PROM2',
+    'ZFP42',
+    'NOTCH1',
+    'KDM5B',
+    'ALDH1A1',
+)
+
+GENE_SETS_TO_PEEK = ('stem', )
+
+GENE_SET_KEYWORDS_TO_PEEK = ('_stem_', ' stem ')
+
+N_JOB = 1
+
+EXTREME_FEATURE_THRESHOLD = 8
+
+N_SAMPLING = 0
+
+N_PERMUTATION = 0
--- code/environment.py
+++ code/environment.py
@@ -3,57 +3,8 @@ import os
 import ccal
 import numpy as np
 import pandas as pd
+from plotly.offline import init_notebook_mode
 
+from make_match_panels import make_match_panels
 
-def make_match_panels(target_x_sample,
-                      feature_x_sample,
-                      n_job,
-                      extreme_feature_threshold,
-                      n_sampling,
-                      n_permutation,
-                      title_prefix,
-                      directory_path,
-                      reset=False):
-
-    for index, target in target_x_sample.iterrows():
-
-        print(index)
-
-        target = target[target != -1]
-
-        file_path_prefix = '{}/{}'.format(directory_path, index)
-
-        scores_file_path = '{}.tsv'.format(file_path_prefix)
-
-        if not reset and os.path.isfile(scores_file_path):
-
-            scores = pd.read_table(scores_file_path, index_col=0)
-
-            scores = scores.loc[feature_x_sample.index]
-
-        else:
-
-            scores = None
-
-        if target.unique().size == 2:
-
-            target_type = 'binary'
-
-        elif 2 < target.unique().size:
-
-            target_type = 'categorical'
-
-        title = '{}{}'.format(title_prefix, index)
-
-        ccal.make_match_panel(
-            target,
-            feature_x_sample,
-            target_ascending=True,
-            scores=scores,
-            n_job=n_job,
-            extreme_feature_threshold=extreme_feature_threshold,
-            n_sampling=n_sampling,
-            n_permutation=n_permutation,
-            target_type=target_type,
-            title=title,
-            file_path_prefix=file_path_prefix)
+init_notebook_mode(connected=True)
