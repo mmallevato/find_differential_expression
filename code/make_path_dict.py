e27c24f2ea6380a4ac065870c9131f39f35dbf73
KwatME <kwatme8@gmail.com>
2019-01-31 14:09:20 -0800

    max_n_job

--- code/make_path_dict.py
+++ code/make_path_dict.py
@@ -5,30 +5,38 @@ from ccal import establish_path
 
 def make_path_dict(setting):
 
-    path_dict = dict(
-        kallisto_directory_path="../output/kallisto",
-        gene_x_sample_file_path="../output/gene_x_sample.processed.tsv",
-        target_x_sample_file_path="../output/target_x_sample.tsv",
-        differentially_expressed_gene_directory_path="../output/differentially_expressed_gene",
-        gene_set_x_sample_file_path="../output/gene_set_x_sample.tsv",
-        differentially_expressed_gene_set_directory_path="../output/differentially_expressed_gene_set",
-        hill_plot_directory_path="../output/hill_plot",
-        expanded_gene_set_directory_path="../output/expanded_gene_set",
-        highlight_directory_path="../output/highlight",
-        mountain_plot_directory_path="../output/mountain_plot",
+    path_dict = {}
+
+    for name in (
+        "kallisto/",
+        "gene_x_sample.processed.tsv",
+        "target_x_sample.tsv",
+        "differentially_expressed_gene/",
+        "gene_set_x_sample.tsv",
+        "differentially_expressed_gene_set/",
+        "hill_plot/",
+        "expanded_gene_set/",
+        "highlight/",
+        "mountain_plot/",
+    ):
+
+        path_dict[name] = "{}/{}".format(setting["output_directory_path"], name)
+
+    for name, path in path_dict.items():
+
+        if name.endswith("/"):
+
+            path_type = "directory"
+
+        else:
+
+            path_type = "file"
+
+        establish_path(path, path_type)
+
+    path_dict["gene_set_file_paths"] = tuple(
+        "{}/{}".format(setting["gene_set_directory_path"], name)
+        for name in sorted(listdir(setting["gene_set_directory_path"]))
     )
-
-    path_dict.update(
-        gene_set_file_paths=tuple(
-            "{}/{}".format(setting["gene_set_directory_path"], file_name)
-            for file_name in sorted(listdir(setting["gene_set_directory_path"]))
-        )
-    )
-
-    for path_name, path in path_dict.items():
-
-        if "directory" in path_name:
-
-            establish_path(path, "directory")
 
     return path_dict
